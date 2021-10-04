dd54b8a28a9753860a3b59d5470d560c5f764213
MARIA LUIZA RIBEIRO TEIXEIRA <21193063@CFP127ALUNO.LOCAL>
2021-10-04 11:15:43 -0300

    Adicionando link para outra tela e mais um campo no cadastro (altura)

--- app/src/main/java/com/example/primeiroapp/LoginActivity.kt
+++ app/src/main/java/com/example/primeiroapp/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.example.primeiroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class LoginActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar!!.hide()
+
+        val tvHello = findViewById<TextView>(R.id.tv_criar_conta)
+        tvHello.setOnClickListener {
+            val abrirActivityCadastro = Intent(this, CadastroUsuarioActivity::class.java)
+            //contexto e "destino" ||||| ::class.java - instancia a classe
+
+            startActivity(abrirActivityCadastro)
+        }
     }
+
 }
