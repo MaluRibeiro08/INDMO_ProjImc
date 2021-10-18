c838853a3d547a58e057a6c8c35255eaeb3c9fd4
MARIA LUIZA RIBEIRO TEIXEIRA <21193063@CFP127ALUNO.LOCAL>
2021-10-18 11:16:39 -0300

    Validação dos campos - arrumar data nascimento

--- app/src/main/java/com/example/primeiroapp/CadastroUsuarioActivity.kt
+++ app/src/main/java/com/example/primeiroapp/CadastroUsuarioActivity.kt
@@ -1,20 +1,41 @@
 package com.example.primeiroapp
 
 import android.app.DatePickerDialog
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
+import com.google.android.material.textfield.TextInputLayout
 import java.util.*
 
 class CadastroUsuarioActivity : AppCompatActivity()
 {
+    lateinit var editEmail: EditText
+    lateinit var editSenha: EditText
+    lateinit var editNome: EditText
+    lateinit var editProfissao: EditText
+    lateinit var editAltura: EditText
+    lateinit var editDataNascimento : EditText
+    lateinit var radioButtonFem : RadioButton
+    lateinit var radioButtonMasc : RadioButton
+    lateinit var radioGrupSexo : RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_usuario)
+
+        editEmail = findViewById(R.id.edit_text_email_cadastro)
+        editSenha = findViewById(R.id.edit_text_senha_cadastro)
+        editNome = findViewById(R.id.edit_text_nome)
+        editProfissao = findViewById(R.id.edit_text_profissao)
+        editAltura = findViewById(R.id.edit_text_altura)
+        editDataNascimento = findViewById(R.id.edit_text_data_nascimento)
+        radioButtonFem = findViewById<RadioButton>(R.id.radio_button_fem)
+        radioButtonMasc = findViewById<RadioButton>(R.id.radio_button_masc)
+        radioGrupSexo = findViewById<RadioGroup>(R.id.radio_group_sexo)
 
         supportActionBar!!.title = "Perfil"
 
@@ -28,7 +49,7 @@ class CadastroUsuarioActivity : AppCompatActivity()
         val dia = calendario.get(Calendar.DAY_OF_MONTH) // ano
 
         //abrir date picker
-        val etDataNascimento = findViewById<EditText>(R.id.et_data_nascimento)
+        val etDataNascimento = findViewById<EditText>(R.id.edit_text_data_nascimento)
         etDataNascimento.setOnClickListener {
             val dp = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{ view, _ano, _mes, _diaDoMes -> etDataNascimento.setText("$_diaDoMes/${_mes+1}/$_ano")}, ano, mes, dia)
             //Argumentos para datePickerDialog:
@@ -37,7 +58,8 @@ class CadastroUsuarioActivity : AppCompatActivity()
                 // - data, mes e ano que estarão no calendário ao abrir.
 
             dp.show()
-        }
+
+            }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean
@@ -48,13 +70,57 @@ class CadastroUsuarioActivity : AppCompatActivity()
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean
     {
-        when(item.itemId)
-        {
-            R.id.menu_iten_salvar ->
-            {
+            if(validarCampos()) {
                 Toast.makeText(this, "Perfil Salvo", Toast.LENGTH_SHORT).show()
             }
-        }
+            else {
+//                    Toast.makeText(this, "Complete as informações", Toast.LENGTH_SHORT).show()
+            }
+
         return true;
+    }
+
+    fun validarCampos() : Boolean
+    {
+        var validos = true
+
+        if (editEmail.text.isEmpty())
+        {
+            editEmail.error = "Email é obrigatório"
+            validos = false
+        }
+        if (editSenha.text.isEmpty())
+        {
+            editSenha.error = "Senha é obrigatório"
+            validos = false
+        }
+        if (editNome.text.isEmpty())
+        {
+            editNome.error = "Nome é obrigatório"
+            validos = false
+        }
+        if (editAltura.text.isEmpty())
+        {
+            editAltura.error = "Altura é obrigatório"
+            validos = false
+        }
+        if (editProfissao.text.isEmpty())
+        {
+            editProfissao.error = "Profissão é obrigatório"
+            validos = false
+        }
+        if (editDataNascimento.text.isEmpty())
+        {
+            editDataNascimento.error = "Data de nascimento é obrigatório"
+            validos = false
+        }
+        if (!radioButtonFem.isChecked && !radioButtonMasc.isChecked)
+        {
+            Toast.makeText(this, "Escolha uma opção", Toast.LENGTH_SHORT).show()
+            validos = false
+        }
+
+
+        return validos
     }
 }
